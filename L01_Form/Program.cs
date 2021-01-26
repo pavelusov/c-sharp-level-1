b3b78eb4bc6c2e80c44568053a804398f778fd84
pavel.usov <pavel.usov@motorsport.com>
2021-01-26 20:57:38 +0500

    Homework 3 was done

--- L01_Form/Program.cs
+++ L01_Form/Program.cs
@@ -21,39 +21,90 @@ namespace L01_Form
             return weightByKilo / (heightByMeters * heightByMeters);
         }
 
-        static void Main(string[] args)
+        static void HomeWork01_02()
         {
             float ONE_HUNDRED_METERS = 100;
             string firstName, lastName;
             int age, height, weight;
             float heightByMeters;
 
-            Console.WriteLine("Write your first name:");
+            Console.WriteLine("-= Homework 1 =-");
+            Console.WriteLine("Enter your first name:");
 
             firstName = Console.ReadLine();
 
-            Console.WriteLine("Write your last name:");
+            Console.WriteLine("Enter your last name:");
 
             lastName = Console.ReadLine();
 
-            Console.WriteLine("Write your age:");
+            Console.WriteLine("Enter your age:");
 
             age = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Write your height by cm:");
+            Console.WriteLine("Enter your height by cm:");
 
             height = int.Parse(Console.ReadLine());
             heightByMeters = (float)height / (float)ONE_HUNDRED_METERS;
 
 
-            Console.WriteLine("Write your weight by kilo:");
+            Console.WriteLine("Enter your weight by kilo:");
 
             weight = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"Name: {firstName} {lastName}. Age: {age}. Height: {height}. Weight: {weight}. ");
 
-            Console.WriteLine($"Your body mass index: {getBodyMassIndex(weight, heightByMeters)}");
+            Console.WriteLine("-= Homework 2 =-");
 
+            Console.WriteLine($"Your body mass index: {getBodyMassIndex(weight, heightByMeters)}");
+        }
+
+        static float getDistance(float x1, float x2, float y1, float y2)
+        {
+            float powX = (float)Math.Pow(x2 - x1, 2);
+            float powY = (float)Math.Pow(y2 - y1, 2);
+            return (float)Math.Sqrt(powX + powY);
+        }
+
+        static void HomeWork03()
+        {
+            const string Format = "Distance between points {0:F2}";
+
+            float x1, x2, y1, y2;
+
+            Console.WriteLine("\n\n-= Homework 3 =-");
+
+            Console.WriteLine("Enter coordinates of point 1:");
+
+            Console.Write("x1 = ");
+
+            x1 = float.Parse(Console.ReadLine());
+
+            Console.Write("y1 = ");
+
+            y1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinates of point 2:");
+
+            Console.Write("x2 = ");
+
+            x2 = float.Parse(Console.ReadLine());
+
+            Console.Write("y2 = ");
+
+            y2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(Format, getDistance(x1, x2, y1, y2));
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            HomeWork01_02();
+
+            HomeWork03();
+
+            Console.ReadKey();
         }
     }
 }
